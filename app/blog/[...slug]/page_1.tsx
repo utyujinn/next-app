4a0fb445fe5f8005b429e602b8595bc8c03ce08f
utyujinn <hg33fah9@utyujin.com>
2025-08-03 16:29:45 +0900

    add static func

--- app/blog/[...slug]/page_1.tsx
+++ app/blog/[...slug]/page_1.tsx
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import path from 'path';
+import Posts from "@/lib/Posts"
+import "./layout.css"
 
 export async function generateStaticParams() {
   const postsDirectory = path.join(process.cwd(), 'app/blog/md');
@@ -28,7 +30,20 @@ export async function generateStaticParams() {
   });
 }
 
-export default async function Page(){
+export default async function Page({
+  params,
+}: {
+  params: { 
+    slug: string[];
+  }
+}) {
+  const { slug } = params;
+  const decodedSlug = slug.map(segment => decodeURIComponent(segment));
+  const postPath = decodedSlug.join("/");
   return <>
+  <h1>{decodedSlug[slug.length-1]}</h1>
+  <div className="container">
+  <Posts filename={postPath}/>
+  </div>
   </>
 }
